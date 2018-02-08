70846a0d4491908471be0f5aa588bf009d5f7352
Dewang Shah <contact.dewang@gmail.com>
2018-02-08 11:03:15 +0900

    Add kentico typescript sdk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {
+    DeliveryClient,
+    DeliveryClientConfig
+} from 'kentico-cloud-delivery-typescript-sdk';
 
 import { AppComponent } from './app.component';
 
+export function DeliveryClientFactory() {
+    const previewOptions = {
+        enablePreviewMode: true,
+        previewApiKey: 'YOUR API KEY'
+    };
+
+    const projectId = 'YOUR PROJECT ID';
+
+    return new DeliveryClient(
+        new DeliveryClientConfig(projectId, [], previewOptions)
+    );
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule],
+    providers: [
+        {
+            provide: DeliveryClient,
+            useFactory: DeliveryClientFactory,
+            deps: []
+        }
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
